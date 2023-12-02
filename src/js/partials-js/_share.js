0ffab0583f43ab52cde50cfb673835e665da96bf
Vir19 <viir19@hotmail.com>
2023-12-02 19:32:11 +0100

    Js-colour-button-done

---
+++ src/js/partials-js/_share.js
@@ -0,0 +1,16 @@
+//variables a traer : botón compartir
+const createButton = document.querySelector('.js_createButton');
+
+createButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  createButton.classList.toggle('clicked');
+});
+
+
+
+
+/* 
+
+
+*/
+
