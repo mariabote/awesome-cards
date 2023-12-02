08b3a10fc89ae9a1e24530775c73a667db5bc815
Marta <martablancoorte@gmail.com>
2023-12-02 15:33:45 +0100

    partials js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+import './partials-js/_variables';
+import './partials-js/_collapsables';
+import './partials-js/_design';
+import './partials-js/_fill';
+import './partials-js/_share';
+import './partials-js/_preview';
