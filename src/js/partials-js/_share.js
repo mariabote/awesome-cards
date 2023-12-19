2592e45ac9e5ea839d63b437c199e1a33910ed2d
Vir19 <viir19@hotmail.com>
2023-12-19 18:09:00 +0100

    Finish twitter intent button

--- src/js/partials-js/_share.js
+++ src/js/partials-js/_share.js
@@ -1,6 +1,7 @@
 //  VARIABLES
 const createButton = document.querySelector('.js_createButton');
 const cardCreated = document.querySelector('.js_cardCreated');
+const twitterButton = document.querySelector('.js__twitterButton');
 
 //  FUNCIONES
 function toggleCardCreated() {
@@ -13,3 +14,15 @@ createButton.addEventListener('click', (event) => {
   toggleCardCreated();
 }); 
 
+twitterButton.addEventListener('click', (event) => {
+
+  // URL y texto que a compartir en Twitter
+  const shareUrl = 'https://awesome-profile-card.com?id=A456DF0001';
+  const shareText = '¡He creado mi nueva tarjeta profesional con Awesome Profile Cards!';
+
+  // Construir el enlace de Twitter con los parámetros necesarios
+  const twitterLink = `https://twitter.com/intent/tweet?url=${encodeURIComponent(shareUrl)}&text=${encodeURIComponent(shareText)}`;
+
+  // Abrir una nueva ventana emergente con el enlace de Twitter
+  window.open(twitterLink, '_blank', 'width=600,height=600');
+});
